54382ef4c60666b6524c76e0603340f42661f690
RafiKastner <rafikastner@gmail.com>
2025-04-05 10:29:02 -0400

    Testing dropdown

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Dropdown, DropdownItem, DropdownContextProvider} from '@reactjs-os/dropdown';
+
 
 function App() {
+  console.log(DropdownContextProvider) //idk why this isnt exporting right
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DropdownContextProvider>
+      <Dropdown inner="Hello">
+        <DropdownItem text="bruh"/>
+      </Dropdown>
+    </DropdownContextProvider>
   );
 }
 
